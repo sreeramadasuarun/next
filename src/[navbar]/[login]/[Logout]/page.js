17eefcae6b16d34f67a43a8e8ca11ca8fb9e9976
naveenmech <vas.naveenkumar99@gmail.com>
2023-05-17 18:31:04 +0530

    add utilities

---
+++ src/[navbar]/[login]/[Logout]/page.js
@@ -0,0 +1,32 @@
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+// const Logout = () => {
+//   const navigate = useNavigate();
+//   return (
+//     <div className="center mt-[3.6rem]">
+//       <h1>Logout</h1>
+//       <button className="btn" onClick={() => navigate("/")}>
+//         back to home page
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Logout;
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Logout = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="center mt-[3.6rem]">
+      <h1>Logout</h1>
+      <button className="btn" onClick={() => navigate("/")}>
+        back to home page
+      </button>
+    </div>
+  );
+};
+
+export default Logout;
