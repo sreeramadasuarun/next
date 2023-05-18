429208adca3a0a47c35b3ef787d557bd121c455b
sreeramadasuarun <sreeramadasuarun@gmail.com>
2023-05-18 23:35:22 +0530

    deploy testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,9 +2,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import SideBar from "./components/SideBar";
-import { useState, useEffect, Fragment } from "react";
+import React from "react";
 import TopBar from "./components/TopBar";
-import { Transition } from "@headlessui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,52 +13,14 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const [showNav, setShowNav] = useState(true);
-  const [isMobile, setIsMobile] = useState(false);
-
-  function handleResize() {
-    if (innerWidth <= 640) {
-      setShowNav(false);
-      setIsMobile(true);
-    } else {
-      setShowNav(true);
-      setIsMobile(false);
-    }
-  }
-
-  useEffect(() => {
-    if (typeof window != undefined) {
-      addEventListener("resize", handleResize);
-    }
-
-    return () => {
-      removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <>
       <html lang="en">
         <body className="flex flex-col">
           <>
-            <TopBar showNav={showNav} setShowNav={setShowNav} />
-            <Transition
-              as={Fragment}
-              show={showNav}
-              enter="transform transition duration-[400ms]"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transform duration-[400ms] transition ease-in-out"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <SideBar showNav={showNav} />
-            </Transition>
-            <main
-              className={`pt-16 transition-all duration-[400ms] ${
-                showNav && !isMobile ? "pl-56" : ""
-              }`}
-            >
+            <TopBar />
+            <SideBar />
+            <main className="pt-16 pl-56 transition-all duration-[400ms]">
               <div>{children}</div>
             </main>
           </>
